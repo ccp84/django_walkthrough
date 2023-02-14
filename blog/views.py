9759c116248cd067dcacce197cd8450b9c8dbd33
ccp84 <ccp84@icloud.com>
2023-02-14 21:56:25 +0000

    py>debug off

--- blog/views.py
+++ blog/views.py
@@ -69,11 +69,11 @@ class PostDetail(View):
 class PostLike(View):
 
     def post(self, request, slug, *args, **kwargs):
-        post = get_object_or_404(queryset, slug=slug)
+        post = get_object_or_404(Post, slug=slug)
 
         if post.likes.filter(id=request.user.id).exists():
             post.likes.remove(request.user)
         else:
             post.likes.add(request.user)
 
-        return HttpResponseRedirect(reverse('post_detal', args=[slug]))
+        return HttpResponseRedirect(reverse('post_detail', args=[slug]))
