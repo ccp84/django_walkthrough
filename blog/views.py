a7e72b2653131daad1becda6384112ba923c8942
ccp84 <ccp84@icloud.com>
2023-02-14 14:05:26 +0000

    html>copied crispy templates

--- blog/views.py
+++ blog/views.py
@@ -27,7 +27,7 @@ class PostDetail(View):
             {
                 "post": post,
                 "comments": comments,
-                "liked": liked
+                "liked": liked,
                 "comment_form": CommentForm()
             }
         )
